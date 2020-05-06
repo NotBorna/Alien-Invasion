eaa42c0188ca630e4cb6b2902e51445b73411408
NotBorna <64820329+NotBorna@users.noreply.github.com>
2020-05-06 10:10:26 -0700

    Update main.py

--- Alien Invasion/main.py
+++ Alien Invasion/main.py
@@ -10,20 +10,21 @@ pygame.mixer.music.play(-1)
 
 player_gun = pygame.mixer.Sound("player_gun.wav")
 enemy_death = pygame.mixer.Sound("enemy_death.wav")
-
+medsound = pygame.mixer.Sound("medsound.wav")
+player_hit = pygame.mixer.Sound("player_hit.wav")
+gameover = pygame.mixer.Sound("gameover.wav")
 
 pygame.font.init()
 
-width, height = 750, 750
+width, height = 1000, 780
 score = -100
 highscore = 0
+COOLDOWN = 45
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Alien Invasion")
 
 bg = pygame.transform.scale(pygame.image.load(os.path.join("assets", "space.png")),(width, height))
-
-tutorial = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tutorial.png")), (width, height))
 
 player = pygame.image.load(os.path.join("assets", "player.png"))
 
@@ -35,13 +36,13 @@ enemy4 = pygame.image.load(os.path.join("assets", "enemy4.png"))
 player_laser = pygame.image.load(os.path.join("assets", "player_laser.png"))
 enemy_laser = pygame.image.load(os.path.join("assets", "enemy_laser.png"))
 
-class Player:
+medkit = pygame.transform.scale(pygame.image.load(os.path.join("assets", "medkit.png")),(80, 60))
 
-    COOLDOWN = 45
+class Player:
      
     def __init__(self):
     
-        self.x = 375
+        self.x = 430
         self.y = 620
         self.health = 60
         self.img = player
@@ -75,7 +76,7 @@ class Player:
                             self.lasers.remove(laser)
 
     def cooldown(self):
-        if self.cool_down_counter >= self.COOLDOWN:
+        if self.cool_down_counter >= COOLDOWN:
             self.cool_down_counter = 0
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
@@ -96,6 +97,7 @@ class Player:
     
 
 class Enemy:
+    
     color_map = {
                 "purple": (enemy1),
                 "green": (enemy2),
@@ -125,6 +127,7 @@ class Enemy:
             if laser.off_screen(height):
                 self.lasers.remove(laser)
             elif laser.collision(obj):
+                player_hit.play()
                 obj.health -= 10
                 self.lasers.remove(laser)
 
@@ -153,6 +156,30 @@ class Laser:
     def collision(self, obj):
         return collide(self, obj)
 
+
+class Medkit:
+
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = medkit
+        self.mask = pygame.mask.from_surface(self.img)
+
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def heal(self, player):
+        player.health += 20
+        if player.health > 60:
+            player.health = 60
+        
+
+
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
@@ -163,7 +190,7 @@ def main():
 
     global highscore
     fps = 60
-    user_vel = 5
+    user_vel = 6
     run = True
     clock = pygame.time.Clock()
     wave = 0
@@ -172,11 +199,14 @@ def main():
     lost_font = pygame.font.SysFont("comicsans", 100)
     user = Player()
     enemies = []
-    wave_lenght = 2
+    wave_lenght = 3
     enemy_vel = 1
     lost = False
-    laser_vel = 8
+    laser_vel = 9
     lost_count = 0
+    med_vel = 2
+    medkits = []
+    global COOLDOWN
 
 
     def redraw():
@@ -192,6 +222,8 @@ def main():
         win.blit(back_text, (width - 120, 10))
         for enemy in enemies:
             enemy.draw(win)
+        for medkit in medkits:
+            medkit.draw(win)
         user.draw(win)
         if lost:
             lost_label = lost_font.render("Game Over", 1, (255, 255, 255))
@@ -205,16 +237,21 @@ def main():
 
         redraw()
 
+        if random.randrange(0, fps*90) == 1:
+            medkit = Medkit(random.randrange(70, width - 70), -100)
+            medkits.append(medkit)
+            
         if score > highscore:
             highscore = score
 
         
         if user.health <= 0:
             lost = True
+            gameover.play()
             lost_count += 1
 
         if lost:
-            if lost_count > fps*3:
+            if lost_count > fps*2:
                 score = -100
                 run = False
                 
@@ -224,7 +261,7 @@ def main():
         
 
         if len(enemies) == 0:
-            wave_lenght += 2
+            wave_lenght += 3
             wave += 1
             score += 100
             for i in range(wave_lenght):
@@ -257,11 +294,26 @@ def main():
         if keys[pygame.K_SPACE]: # shoot with space
             user.shoot()
 
+
+        for medkit in medkits[:]:
+            
+            medkit.move(med_vel)
+            
+            if collide(medkit, user):
+                score += 10
+                medsound.play()
+                medkits.remove(medkit)
+                medkit.heal(user)
+
+            elif medkit.y > height:
+                medkits.remove(medkit)
+                    
+
         
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
             enemy.move_lasers(laser_vel, user)
-            if random.randrange(0, 60*3) ==1:
+            if random.randrange(0, fps*3) == 1:
                 enemy.shoot()
             if collide(enemy, user):
                 enemy_death.play()
@@ -284,11 +336,12 @@ def menu():
         highscore_label = hs_font.render(f"HighScore: {highscore}", 1, (255,255,255))
         title_label = title_font.render("Alien Invasion", 1, (200,0,0))
         start_label = title_font.render("(S)tart", 1, (0,255,0))
-        tutorial_label = title_font.render("(T)utorial", 1, (0,200,200))
-        win.blit(start_label, (width/2 - start_label.get_width()/2, 350))
+        start_label = title_font.render("(S)tart", 1, (0,255,0))
+        instruction_label = title_font.render("(I)nstructions", 1, (200,0,200))
+        win.blit(start_label, (width/2 - start_label.get_width()/2, 330))
         win.blit(highscore_label, (20, 20))
-        win.blit(tutorial_label, (width/2 - start_label.get_width()/2, 500))
         win.blit(title_label, (width/2 - title_label.get_width()/2, 150))
+        win.blit(instruction_label, (width/2 - start_label.get_width()/2, 480))
         score = -100
         pygame.display.update()
 
@@ -296,15 +349,30 @@ def menu():
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                quit()
                 
         if keys[pygame.K_s]:
                 main()
 
-        if keys[pygame.K_t]:
-            win.blit(tutorial, (0,0))
-            pygame.display.update()
-            time.sleep(3)
+        if keys[pygame.K_i]:
+                win.blit(bg, (0,0))
+                title_label1 = title_font.render("Use arrow keys to move", 1, (255,255,255))
+                title_label2 = title_font.render("and space to shoot", 1, (255,255,255))
+                title_label3 = title_font.render("health = 60", 1, (255,255,255))
+                title_label4 = title_font.render("lasers - 10 health", 1, (255,255,255))
+                title_label5 = title_font.render("aliens - 20 health", 1, (255,255,255))
+                title_label6 = title_font.render("medkits + 20 health", 1, (255,255,255))
+                win.blit(title_label1, (width/2 - title_label1.get_width()/2, 50))
+                win.blit(title_label2, (width/2 - title_label2.get_width()/2, 120))
+                win.blit(title_label3, (width/2 - title_label3.get_width()/2, 190))
+                win.blit(title_label4, (width/2 - title_label4.get_width()/2, 260))
+                win.blit(title_label5, (width/2 - title_label5.get_width()/2, 330))
+                win.blit(title_label6, (width/2 - title_label6.get_width()/2, 400))
+                pygame.display.update()          
+                pygame.time.delay(5000)
+                
+
+        
 
             
 
